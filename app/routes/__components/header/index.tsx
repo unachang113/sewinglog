faddbfdf3fd2cd08a5ea38b666a81c8b199d427a
unachang113 <hita.d.s2.c@gmail.com>
2024-05-12 22:53:32 +0900

    feat: ヘッダーのロゴクリックでトップページに遷移するように修正

--- app/routes/__components/header/index.tsx
+++ app/routes/__components/header/index.tsx
@@ -1,7 +1,11 @@
+import {Link} from '@remix-run/react';
+
 export function Header() {
   return (
     <header className="px-4 py-4 w-full border-b-2 border-primary bg-white">
-      <h1 className="font-bold text-2xl text-center font-logo text-primary">Sewing log</h1>
+      <h1 className="font-bold text-2xl text-center font-logo text-primary">
+        <Link to="/">Sewing log</Link>
+      </h1>
     </header>
   );
 }
