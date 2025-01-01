03167077ae01977396b83727020150de7ade88bc
unachang113 <hita.d.s2.c@gmail.com>
2025-01-01 23:24:11 +0900

    feature: feature flagの設定追加

--- __tests__/setup-test.ts
+++ __tests__/setup-test.ts
@@ -1,5 +1,2 @@
 /* eslint-disable-next-line import/no-unassigned-import */
 import '@testing-library/jest-dom';
-import {installGlobals} from '@remix-run/node';
-
-installGlobals();
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import {vitePlugin as remix} from '@remix-run/dev';
-import {installGlobals} from '@remix-run/node';
 import {defineConfig} from 'vite';
 import {vercelPreset} from '@vercel/remix/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-installGlobals();
-
 export default defineConfig({
-  plugins: [remix({presets: [vercelPreset()]}), tsconfigPaths()],
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    remix({
+      presets: [vercelPreset()],
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        unstable_singleFetch: true,
+      },
+    }),
+    tsconfigPaths()
+  ],
   test: {
     environment: 'jsdom',
     globals: true,
